f7b33c28aa977ef7a63a641d287f7543401baa00
harrisafoster <harrisafoster@gmail.com>
2021-05-06 15:10:36 +0200

    Testes initiaux pour concevoir l'algorithme.

--- bruteforce.py
+++ bruteforce.py
@@ -1,1 +1,20 @@
-print('initial commit test')
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('sheet1.csv')
+
+sorted_df = df.sort_values(by=['2_year_earnings'], ascending=False)
+
+sorted_df['amount_earned'] = sorted_df['cost'] * sorted_df['2_year_earnings']
+
+budget = 500
+spent = 0
+stocks_to_buy = []
+for value in sorted_df['cost']:
+    if spent < 500 and (spent + value) <= 500:
+        spent += value
+        stocks_to_buy.append(value)
+
+print(spent)
+print(stocks_to_buy)
+print(sorted_df)
