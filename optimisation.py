c220d98b7c686e8a9201c35070d6b92ce2ffceb4
harrisafoster <harrisafoster@gmail.com>
2021-05-31 17:01:26 +0200

    Ajout d'un condition if __name__ = '__main__' pour faire tourner le programme au lancement. Ajout des options basiques de menu d'utilisateur pour choisir le dataset et un budget customisé. Ajout des commentaires et docstrings

--- optimisation.py
+++ optimisation.py
@@ -1,8 +1,14 @@
 import pandas as pd
+from os import listdir
 
 
 class Optimisation:
     def __init__(self, datasheet, budget=500):
+        """
+        Initialisation of object
+        :param datasheet: .csv format datasheet
+        :param budget: by default 500, can be modified
+        """
         self.df = pd.read_csv(datasheet)
         self.budget = budget
 
@@ -18,6 +24,10 @@ class Optimisation:
         return sorted_df
 
     def make_item_list(self):
+        """
+        .make_item_list creates a list of dict values containing the names, costs, and amounts earned of all stocks
+        :return: list of dict variables containing stock information
+        """
         list_of_items = []
         for index, row in self.add_profit_column().iterrows():
             new_item = {'name': row['name'], 'price': row['price'], 'amount_earned': row['amount_earned']}
@@ -25,6 +35,11 @@ class Optimisation:
         return list_of_items
 
     def determine_optimal_investments(self, df):
+        """
+        .determine_optimal_investments tests all possible combinations of stocks to determine the optimal solution.
+        :param df: Dataframe that has already been filtered and sorted goes here
+        :return: total cost, total earnings, and data on the chosen stocks
+        """
         amount_earned = df['amount_earned'].to_list()
         price = df['price'].to_list()
         rounded_multiplied_budget = self.budget * 100  # rmb
@@ -33,6 +48,15 @@ class Optimisation:
         combination_matrix = [[-1 for i in range(rounded_multiplied_budget + 1)] for j in range(index + 1)]
 
         def check_all_combinations(internal_price, internal_earned, internal_rmb, internal_index):
+            """
+            Dynamic function that checks all possible combinations based on given parameters.
+            :param internal_price: list of prices of stocks
+            :param internal_earned: list of possibles earnings of stocks
+            :param internal_rmb: budget multiplied by 100 and rounded in order to avoid errors resultant of FLOAT type
+            variables
+            :param internal_index: total amount of stocks to be examined
+            :return: optimal maximum profit found in decision matrix
+            """
             if internal_index == 0 or internal_rmb == 0:
                 return 0
             if combination_matrix[internal_index][internal_rmb] != -1:
@@ -52,6 +76,7 @@ class Optimisation:
                                                                                           internal_index - 1)
                 return combination_matrix[internal_index][internal_rmb]
 
+        # Reconstruction of list of stocks chosen based on the amount_earned and the price constraints
         final_result = check_all_combinations(price, amount_earned, rounded_multiplied_budget, index)
         rmb = rounded_multiplied_budget
         prices = []
@@ -76,13 +101,34 @@ class Optimisation:
         return sum(prices), sum(amounts_earned), final_items
 
 
-test_20 = Optimisation('datasets/sheet1.csv')
-test_dataset1 = Optimisation('datasets/dataset1.csv')
-test_dataset2 = Optimisation('datasets/dataset2.csv')
-test_dataset1_sample = Optimisation('datasets/dataset1_sample.csv')
-test_dataset2_sample = Optimisation('datasets/dataset2_sample.csv')
-
-print(test_20.determine_optimal_investments(test_20.add_profit_column()))
-print(test_dataset1_sample.determine_optimal_investments(test_dataset1_sample.add_profit_column()))
-# print(test_dataset1.determine_optimal_investments(test_dataset1.add_profit_column()))
-# print(test_dataset2.determine_optimal_investments(test_dataset2.add_profit_column()))
+if __name__ == "__main__":
+    print('For which dataset would you like an optimised investment solution?', '\n', listdir('datasets'))
+    while True:
+        choice = input()
+        try:
+            assert choice in listdir('datasets')
+        except AssertionError:
+            print('Please select one of the available options.')
+            continue
+        else:
+            break
+    print("The default budget is 500€, would you like to enter a custom budget? (y/n)")
+    custom_budget_choice = input()
+    if custom_budget_choice == 'y':
+        print("What is the client's maximum budget?")
+        while True:
+            budget_choice = input()
+            try:
+                int(budget_choice)
+            except ValueError:
+                print('Please enter a whole number (ex.700).')
+                continue
+            else:
+                data_to_be_analyzed = Optimisation(('datasets/' + choice), int(budget_choice))
+                print("Format: total_spent, total_earned, stocks_to_buy", '\n',
+                      data_to_be_analyzed.determine_optimal_investments(data_to_be_analyzed.add_profit_column()))
+                break
+    if custom_budget_choice != 'y':
+        data_to_be_analyzed = Optimisation(('datasets/' + choice))
+        print("Format: total_spent, total_earned, stocks_to_buy", '\n',
+              data_to_be_analyzed.determine_optimal_investments(data_to_be_analyzed.add_profit_column()))
