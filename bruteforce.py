d9a318d002ce621077684f4a8372f39e67ba6749
harrisafoster <harrisafoster@gmail.com>
2021-06-01 13:37:05 +0200

    Ajout des docstrings/commentaires, ajustement de la méthode determine_optimal_investments pour qu'il ne genère plus d'erreurs dûs aux variables FLOAT, Ajout d'une condition if __name__ = "__main__" pour faire tourner le programme au lancement selon le fichier choisi.

--- bruteforce.py
+++ bruteforce.py
@@ -1,8 +1,14 @@
 import pandas as pd
+from os import listdir
 
 
 class BruteForce:
     def __init__(self, datasheet, budget=500):
+        """
+        Initialisation of object
+        :param datasheet: .csv format datasheet
+        :param budget: by default 500, can be modified
+        """
         self.df = pd.read_csv(datasheet)
         self.budget = budget
 
@@ -12,6 +18,10 @@ class BruteForce:
         return sorted_df
 
     def make_items(self):
+        """
+        .make_item_list creates a list of dict values containing the names, costs, and amounts earned of all stocks
+        :return: list of dict variables containing stock information
+        """
         list_of_items = []
         for index, row in self.sort_and_add_earnings().iterrows():
             new_item = {'name': row['name'], 'price': row['price'], 'amount_earned': row['amount_earned'] / 100}
@@ -19,45 +29,84 @@ class BruteForce:
         return list_of_items
 
     def determine_optimal_investments(self, df):
-        price = df['price']
-        amount_earned = df['amount_earned']
-        n = len(amount_earned)
+        """
+        .determine_optimal_investments tests all possible combinations of stocks to determine the optimal solution.
+        :param df: Dataframe that has already been sorted goes here
+        :return: total cost, total earnings, and data on the chosen stocks
+        """
+        price = df['price'].to_list()
+        amount_earned = df['amount_earned'].to_list()
+        index = len(amount_earned)
         prices = []
         amounts_earned = []
+        rmb = self.budget * 100  # rounded multiplied budget
 
-        K = [[0 for x in range(self.budget + 1)] for x in range(n + 1)]
+        combination_matrix = [[0 for x in range(rmb + 1)] for x in range(index + 1)]
 
-        for i in range(n + 1):
-            for w in range(self.budget + 1):
-                if i == 0 or w == 0:
-                    K[i][w] = 0
-                elif price[i - 1] <= w:
-                    K[i][w] = max(amount_earned[i - 1] + K[i - 1][w - price[i - 1]], K[i - 1][w])
+        for i in range(index + 1):
+            for dollar_in_budget in range(rmb + 1):
+                if i == 0 or dollar_in_budget == 0:
+                    combination_matrix[i][dollar_in_budget] = 0
+                elif int(price[i - 1] * 100) <= dollar_in_budget:
+                    combination_matrix[i][dollar_in_budget] \
+                        = max(amount_earned[i - 1]
+                              + combination_matrix[i - 1][dollar_in_budget - int(price[i - 1] * 100)],
+                              combination_matrix[i - 1][dollar_in_budget])
                 else:
-                    K[i][w] = K[i - 1][w]
+                    combination_matrix[i][dollar_in_budget] = combination_matrix[i - 1][
+                        dollar_in_budget]
 
-        res = K[n][self.budget]
-        w = self.budget
-        for i in range(n, 0, -1):
-            if res <= 0:
+        # Reconstruction of list of stocks chosen based on the amount_earned and the price constraints
+        final_result = combination_matrix[index][rmb]
+        reconstruction_budget = rmb
+        for i in range(index, 0, -1):
+            if final_result <= 0:
                 break
-            if res == K[i - 1][w]:
+            if final_result == combination_matrix[i - 1][reconstruction_budget]:
                 continue
             else:
                 prices.append(price[i - 1])
                 amounts_earned.append(amount_earned[i - 1] / 100)
-                res = res - amount_earned[i - 1]
-                w = w - price[i - 1]
+                final_result = final_result - amount_earned[i - 1]
+                reconstruction_budget = reconstruction_budget - int(price[i - 1] * 100)
         final_items = []
         associated_prices = []
-        for i in range(len(prices[:-1])):
+        for i in range(len(prices)):
             associated_prices.append([prices[i], amounts_earned[i]])
         for item in self.make_items():
             if [item['price'], item['amount_earned']] in associated_prices:
                 final_items.append(item)
-        return sum(prices), K[n][self.budget] / 100, final_items
+        return sum(prices), sum(amounts_earned), final_items
 
 
-obj_bruteforce = BruteForce('datasets/sheet1.csv')
-
-print(obj_bruteforce.determine_optimal_investments(obj_bruteforce.sort_and_add_earnings()))
+if __name__ == "__main__":
+    print('For which dataset would you like a Bruteforce investment solution?', '\n', listdir('datasets'))
+    while True:
+        choice = input()
+        try:
+            assert choice in listdir('datasets')
+        except AssertionError:
+            print('Please select one of the available options.')
+            continue
+        else:
+            break
+    print("The default budget is 500€, would you like to enter a custom budget? (y/n)")
+    custom_budget_choice = input()
+    if custom_budget_choice == 'y':
+        print("What is the client's maximum budget?")
+        while True:
+            budget_choice = input()
+            try:
+                int(budget_choice)
+            except ValueError:
+                print('Please enter a whole number (ex.700).')
+                continue
+            else:
+                data_to_be_analyzed = BruteForce(('datasets/' + choice), int(budget_choice))
+                print("Format: total_spent, total_earned, stocks_to_buy", '\n',
+                      data_to_be_analyzed.determine_optimal_investments(data_to_be_analyzed.sort_and_add_earnings()))
+                break
+    if custom_budget_choice != 'y':
+        data_to_be_analyzed = BruteForce(('datasets/' + choice))
+        print("Format: total_spent, total_earned, stocks_to_buy", '\n',
+              data_to_be_analyzed.determine_optimal_investments(data_to_be_analyzed.sort_and_add_earnings()))
